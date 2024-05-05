3cb447d2679c3aa7c72d9e36408e22de26b2356c
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-05-05 13:25:40 -0300

    add deps

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,10 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
